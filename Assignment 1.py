22f2a02ffa808b64837caa26ebc589a24a9258b0
Parag Yadav <126933943+yadavparagyp@users.noreply.github.com>
2023-06-27 15:33:39 +0530

    assignment

---
+++ Assignment 1.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+"""1. In the below elements which of them are values or an expression? eg:- values can be
+integer or string and expressions will be mathematical operators.
+*
+&#39;hello&#39;
+-87.8
+-
+/
++
+6"""
+
+
+# In[4]:
+
+
+#1
+#values = 'hello', -87.8, 6
+#Expression = *, -, /, +
+
+
+# In[5]:
+
+
+#2. What is the difference between string and variable?
+
+#String are the basic input or value in terms of word or statement in between "".For example - "string"
+#Variable are containers were data is stored.for example - Var = "string".
+#Difference = Assigning a string to a variable is (var = 'string')
+
+
+# In[6]:
+
+
+#3. Describe three different data types.
+
+# List- Lists are used to store multiple element in a single variable. Example- List = ["ineuron","datascience",8]
+#tuple- Lists are used to store multiple items in a single variable.here, elements are indexed. Example - tuple = ("ineuron", "data science")
+#Dictonary- Dictionaries are used to store data values in key:value pairs.Items are changeable and ordered. Example- Dict = {"company":"ineuron", "employee":"xyz"}
+
+
+# In[7]:
+
+
+#4. What is an expression made up of? What do all expressions do?
+
+#Expression is made up of variable, operators and functions.For example- (x = 2 y = 3 ; x+y)where x & y are variables, 2 and 3 are int values and + is an mathematical operators.
+#the expression evaluates down to a single or specific outcome.
+
+
+# In[ ]:
+
+
+#5. This assignment statements, like spam = 10. What is the difference between and expression and a statement?
+
+#The assignment statement like spam  = 10 represent command or value to assign whereas exprssion contains variable, operators and function.
+
+
+# In[9]:
+
+
+#6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon+1
+
+#ANS=23
+
+
+# In[11]:
+
+
+#7. What should the values of the following two terms be?
+'spam'+'spamspam'
+'spam'*3
+
+#Ans = 'spamspamspam'
+
+
+# In[12]:
+
+
+#8. Why is eggs a valid variable name while 100 is invalid?
+
+#Because 100 is an integer, means a int datatype and a datatype is can't be used as a variable.
+#Whereas egg is not any datatype.
+
+
+# In[13]:
+
+
+#9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+#int() = integer
+#float() = floting point number
+#str()  = string
+
+
+# In[14]:
+
+
+#10. Why does this expression cause an error? How can you fix?
+'I have eaten' + 99 + 'burritos.'
+
+
+#Ans = can only concatenate str to str not int to str
+
+
+# In[ ]:
+
+
+
+
